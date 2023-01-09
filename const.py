be0e29b9e22e329bf4e1a6aecb0c5e380bcde758
Dominik Amann <dominik.amann@outlook.de>
2023-01-09 20:20:12 +0100

    Inital Implementation
    Fully functional

--- const.py
+++ const.py
@@ -1,9 +1,175 @@
-import logging
+"""Constants for the Ökofen Pellematic Compact integration."""
 
-LOGGER = logging.getLogger(__package__)
+from homeassistant.const import (
+    UnitOfTemperature,
+    UnitOfMass,
+)
 
-DOMAIN = "oekofen-pellematic-compact"
+DOMAIN = "oekofen_pellematic_compact"
+DEFAULT_NAME = "pellematic"
+DEFAULT_SCAN_INTERVAL = 30
+DEFAULT_HOST = "http://192.168.178.91:4321/8n2L/all"
+CONF_SOLAREDGE_HUB = "solaredge_hub"
+ATTR_STATUS_DESCRIPTION = "status_description"
+ATTR_MANUFACTURER = "Ökofen"
+ATTR_MODEL = "Pellematic Compact"
 
-CONF_NAME = "name"
-CONF_URL = "url"
-CONF_UPDATE_INTERVAL = "update_interval"
+SYSTEM_SENSOR_TYPES = {
+    "L_ambient": [
+        "Outside Temperature",
+        "L_ambient",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+}
+
+PE1_SENSOR_TYPES = {
+    "L_storage_max": [
+        "Pellet Storage Level Max",
+        "L_storage_max",
+        UnitOfMass.KILOGRAMS,
+        None,
+    ],
+    "L_storage_fill": [
+        "Pellet Storage Level",
+        "L_storage_fill",
+        UnitOfMass.KILOGRAMS,
+        None,
+    ],
+    "storage_fill_yesterday": [
+        "Pellet Usage Yesterday",
+        "storage_fill_yesterday",
+        UnitOfMass.KILOGRAMS,
+        None,
+    ],
+    "storage_fill_today": [
+        "Pellet Usage Today",
+        "storage_fill_today",
+        UnitOfMass.KILOGRAMS,
+        None,
+    ],
+    "L_storage_popper": [
+        "Pellet Hopper Tank",
+        "L_storage_popper",
+        UnitOfMass.KILOGRAMS,
+        None,
+    ],
+    "L_temp_act": [
+        "Heater Temperature",
+        "L_temp_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_temp_set": [
+        "Heater Temperature set",
+        "L_temp_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_frt_temp_act": [
+        "Heater Flame Chamber Temperature",
+        "L_frt_temp_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_frt_temp_set": [
+        "Heater Flame Chamber Temperature set",
+        "L_frt_temp_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_statetext": ["Pellematic State", "L_statetext", None, "mdi:fire-circle"],
+}
+
+HK1_SENSOR_TYPES = {
+    "L_flowtemp_act": [
+        "Heating Circuit Flow Temperature",
+        "L_flowtemp_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_flowtemp_set": [
+        "Heating Circuit Flow Temperature set",
+        "L_flowtemp_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_statetext": ["Heating Circuit State", "L_statetext", None, "mdi:fire-circle"],
+}
+
+HK1_BINARY_SENSOR_TYPES = {
+    "L_pump": [
+        "Heating Circuit Pump",
+        "L_pump",
+        None,
+        "mdi:pump",
+    ],
+}
+
+PU1_SENSOR_TYPES = {
+    "L_tpo_act": [
+        "Buffer Storage Temperature upper area",
+        "L_tpo_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_tpo_set": [
+        "Buffer Storage Temperature upper area set",
+        "L_tpo_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_tpm_act": [
+        "Buffer Storage Temperature lower area",
+        "L_tpm_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_tpm_set": [
+        "Buffer Storage Temperature lower area set",
+        "L_tpm_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_statetext": ["Buffer Storage State", "L_statetext", None, "mdi:water-circle"],
+}
+
+PU1_BINARY_SENSOR_TYPES = {
+    "L_pump": [
+        "Buffer Storage Pump",
+        "L_pump",
+        None,
+        "mdi:pump",
+    ],
+}
+
+WW1_SENSOR_TYPES = {
+    "L_temp_set": [
+        "Hot Water Circuit Temperature set ",
+        "L_temp_set",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_ontemp_act": [
+        "Hot Water Circuit Temperature on",
+        "L_ontemp_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_offtemp_act": [
+        "Hot Water Circuit Temperature off",
+        "L_offtemp_act",
+        UnitOfTemperature.CELSIUS,
+        None,
+    ],
+    "L_statetext": ["Hot Water Circuit State", "L_statetext", None, "mdi:fire-circle"],
+}
+
+WW1_BINARY_SENSOR_TYPES = {
+    "L_pump": [
+        "Hot Water Circuit Pump",
+        "L_pump",
+        None,
+        "mdi:pump",
+    ]
+}
---
+++ sensor.py
@@ -0,0 +1,346 @@
+"""The Ökofen Pellematic Compact integration."""
+
+import logging
+from typing import Optional, Dict, Any
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+)
+from .const import (
+    HK1_BINARY_SENSOR_TYPES,
+    PU1_BINARY_SENSOR_TYPES,
+    SYSTEM_SENSOR_TYPES,
+    HK1_SENSOR_TYPES,
+    PE1_SENSOR_TYPES,
+    PU1_SENSOR_TYPES,
+    WW1_BINARY_SENSOR_TYPES,
+    WW1_SENSOR_TYPES,
+    DOMAIN,
+    ATTR_STATUS_DESCRIPTION,
+    ATTR_MANUFACTURER,
+    ATTR_MODEL,
+)
+
+from homeassistant.const import (
+    CONF_NAME,
+    UnitOfEnergy,
+    UnitOfTemperature,
+    UnitOfMass,
+)
+
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorDeviceClass,
+    SensorStateClass,
+)
+
+from homeassistant.core import callback
+from homeassistant.util import dt as dt_util
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Setup entry"""
+
+    hub_name = entry.data[CONF_NAME]
+    hub = hass.data[DOMAIN][hub_name]["hub"]
+
+    device_info = {
+        "identifiers": {(DOMAIN, hub_name)},
+        "name": hub_name,
+        "manufacturer": ATTR_MANUFACTURER,
+        "model": ATTR_MODEL,
+    }
+
+    entities = []
+
+    for sensor_info in SYSTEM_SENSOR_TYPES.values():
+        sensor = PellematicSensor(
+            hub_name,
+            hub,
+            device_info,
+            "system",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in HK1_SENSOR_TYPES.values():
+        sensor = PellematicSensor(
+            hub_name,
+            hub,
+            device_info,
+            "hk1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in PE1_SENSOR_TYPES.values():
+        sensor = PellematicSensor(
+            hub_name,
+            hub,
+            device_info,
+            "pe1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in PU1_SENSOR_TYPES.values():
+        sensor = PellematicSensor(
+            hub_name,
+            hub,
+            device_info,
+            "pu1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in WW1_SENSOR_TYPES.values():
+        sensor = PellematicSensor(
+            hub_name,
+            hub,
+            device_info,
+            "ww1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in WW1_BINARY_SENSOR_TYPES.values():
+        sensor = PellematicBinarySensor(
+            hub_name,
+            hub,
+            device_info,
+            "ww1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in WW1_BINARY_SENSOR_TYPES.values():
+        sensor = PellematicBinarySensor(
+            hub_name,
+            hub,
+            device_info,
+            "ww1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in HK1_BINARY_SENSOR_TYPES.values():
+        sensor = PellematicBinarySensor(
+            hub_name,
+            hub,
+            device_info,
+            "hk1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    for sensor_info in PU1_BINARY_SENSOR_TYPES.values():
+        sensor = PellematicBinarySensor(
+            hub_name,
+            hub,
+            device_info,
+            "pu1",
+            sensor_info[0],
+            sensor_info[1],
+            sensor_info[2],
+            sensor_info[3],
+        )
+        entities.append(sensor)
+
+    async_add_entities(entities)
+
+    return True
+
+
+class PellematicBinarySensor(BinarySensorEntity):
+    """Representation of a binary sensor entity."""
+
+    def __init__(self, platform_name, hub, device_info, prefix, name, key, unit, icon):
+        """Initialize the sensor."""
+        self._platform_name = platform_name
+        self._hub = hub
+        self._prefix = prefix
+        self._key = key
+        self._name = name
+        self._unit_of_measurement = unit
+        self._icon = icon
+        self._device_info = device_info
+        self._attr_device_class = BinarySensorDeviceClass.POWER
+
+    @callback
+    def _api_data_updated(self):
+        self.async_write_ha_state()
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the sensor."""
+        current_value = None
+        try:
+            current_value = self._hub.data[self._prefix][self._key]
+        except:
+            return current_value
+        return current_value
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        self._hub.async_add_pellematic_sensor(self._api_data_updated)
+
+    async def async_will_remove_from_hass(self) -> None:
+        self._hub.async_remove_pellematic_sensor(self._api_data_updated)
+
+    @callback
+    def _api_data_updated(self):
+        self.async_write_ha_state()
+
+    @callback
+    def _update_state(self):
+        current_value = None
+        try:
+            current_value = self._hub.data[self._prefix][self._key]
+        except:
+            self._attr_is_on = current_value
+        self._attr_is_on = current_value
+
+    @property
+    def name(self):
+        """Return the name."""
+        return f"{self._name}"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return f"{self._platform_name}_{self._prefix}_{self._key}"
+
+    @property
+    def icon(self):
+        """Return the sensor icon."""
+        return self._icon
+
+    @property
+    def extra_state_attributes(self):
+        return None
+
+    @property
+    def should_poll(self) -> bool:
+        """Data is delivered by the hub"""
+        return False
+
+    @property
+    def device_info(self) -> Optional[Dict[str, Any]]:
+        return self._device_info
+
+
+class PellematicSensor(SensorEntity):
+    """Representation of an Pellematic sensor."""
+
+    def __init__(self, platform_name, hub, device_info, prefix, name, key, unit, icon):
+        """Initialize the sensor."""
+        self._platform_name = platform_name
+        self._hub = hub
+        self._prefix = prefix
+        self._key = key
+        self._name = name
+        self._unit_of_measurement = unit
+        self._icon = icon
+        self._device_info = device_info
+        if self._unit_of_measurement == UnitOfEnergy.KILO_WATT_HOUR:
+            self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+            self._attr_device_class = SensorDeviceClass.ENERGY
+            self._attr_last_reset = dt_util.utc_from_timestamp(0)
+        if self._unit_of_measurement == UnitOfTemperature.CELSIUS:
+            self._attr_device_class = SensorDeviceClass.TEMPERATURE
+            self._attr_state_class = SensorStateClass.MEASUREMENT
+        if self._unit_of_measurement == UnitOfMass.KILOGRAMS:
+            self._attr_device_class = SensorDeviceClass.WEIGHT
+            self._attr_state_class = SensorStateClass.MEASUREMENT
+
+    async def async_added_to_hass(self):
+        """Register callbacks."""
+        self._hub.async_add_pellematic_sensor(self._api_data_updated)
+
+    async def async_will_remove_from_hass(self) -> None:
+        self._hub.async_remove_pellematic_sensor(self._api_data_updated)
+
+    @callback
+    def _api_data_updated(self):
+        self.async_write_ha_state()
+
+    @callback
+    def _update_state(self):
+        current_value = None
+        try:
+            current_value = self._hub.data[self._prefix][self._key]
+            if self._attr_device_class == SensorDeviceClass.TEMPERATURE:
+                current_value = current_value / 10
+        except:
+            self._state = current_value
+        self._state = current_value
+
+    @property
+    def name(self):
+        """Return the name."""
+        return f"{self._name}"
+
+    @property
+    def unique_id(self) -> Optional[str]:
+        return f"{self._platform_name}_{self._prefix}_{self._key}"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement."""
+        return self._unit_of_measurement
+
+    @property
+    def icon(self):
+        """Return the sensor icon."""
+        return self._icon
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        current_value = None
+        try:
+            current_value = self._hub.data[self._prefix][self._key]
+            if self._attr_device_class == SensorDeviceClass.TEMPERATURE:
+                current_value = current_value / 10
+        except:
+            return current_value
+        return current_value
+
+    @property
+    def extra_state_attributes(self):
+        return None
+
+    @property
+    def should_poll(self) -> bool:
+        """Data is delivered by the hub"""
+        return False
+
+    @property
+    def device_info(self) -> Optional[Dict[str, Any]]:
+        return self._device_info
